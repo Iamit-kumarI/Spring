ec7972f259e26b9d5a3826daab3fb1c1334933da
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-16 23:21:58 +0530

    JWT updated

--- journalAPP/src/main/java/com/engineeringdigest/journalAPP/config/SpringSecurity.java
+++ journalAPP/src/main/java/com/engineeringdigest/journalAPP/config/SpringSecurity.java
@@ -1,12 +1,15 @@
 package com.engineeringdigest.journalAPP.config;
 
+import com.engineeringdigest.journalAPP.filter.JwtFilter;
 import com.engineeringdigest.journalAPP.service.UserDetailserviceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,6 +17,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +25,9 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecurity {
     @Autowired
     private UserDetailserviceImpl userDetailservice;
+
+    @Autowired
+    private JwtFilter jwtFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
         http
@@ -29,8 +36,8 @@ public class SpringSecurity {
                 .authorizeHttpRequests(auth->auth
                 .requestMatchers("/journal/**","/user/**").authenticated()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults());
+                .anyRequest().permitAll());
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 //    @Autowired
@@ -40,5 +47,14 @@ public class SpringSecurity {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
+    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailservice).passwordEncoder(passwordEncoder());
     }
 }
--- journalAPP/src/main/java/com/engineeringdigest/journalAPP/controller/PublicController.java
+++ journalAPP/src/main/java/com/engineeringdigest/journalAPP/controller/PublicController.java
@@ -1,13 +1,33 @@
 package com.engineeringdigest.journalAPP.controller;
 
 import com.engineeringdigest.journalAPP.entity.User;
+import com.engineeringdigest.journalAPP.service.UserDetailserviceImpl;
 import com.engineeringdigest.journalAPP.service.UserService;
+import com.engineeringdigest.journalAPP.utils.JwtUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/public")
 public class PublicController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private UserDetailserviceImpl userDetailservice;
+
     @GetMapping("/health-check")
     public String healthCheck(){
         return "OK";
@@ -15,9 +35,23 @@ public class PublicController {
 
     @Autowired
     private UserService userService;
-    @PostMapping("/create-user")
+
+    @PostMapping("/signUp")
     public void createUser(@RequestBody User user){
         userService.saveNewUser(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user){
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
+            UserDetails userDetails=userDetailservice.loadUserByUsername(user.getUserName());
+            String jwt = jwtUtils.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        }catch (Exception e){
+            log.error("Exception in JWT token",e);
+            return new ResponseEntity<>("Incorrect userName or Password",HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
